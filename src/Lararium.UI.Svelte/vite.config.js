92ff7e68033c8c5dc3b7349c730b3e1f024d55ee
IgorShaposhnikov <Igor.Shaposhnikov@urfu.me>
2026-03-18 02:02:13 +0500

    Configurate vite with https support

--- src/Lararium.UI.Svelte/vite.config.js
+++ src/Lararium.UI.Svelte/vite.config.js
@@ -1,9 +1,13 @@
 ﻿import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig, loadEnv } from 'vite';
 import tailwindcss from '@tailwindcss/vite'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 export default defineConfig(({ mode }) => {
     return {
-        plugins: [tailwindcss(), sveltekit()]//, mkcert()],
+        server: {
+            https: true,
+        },
+        plugins: [basicSsl(), tailwindcss(), sveltekit()]
     }
 });
